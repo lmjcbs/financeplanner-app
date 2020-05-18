e24f79c2930290aa50734ccd2ff2615625aa8500
lmjcbs <hello@liamjacobs.dev>
2020-05-18 08:27:38 +0100

    setup intial store

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+
+let initialStore = {
+};
+
+const store = createStore(rootReducer, initialStore);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
